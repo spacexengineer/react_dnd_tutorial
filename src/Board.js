af479dd534ab179f325fcb56a9d35734a0cce636
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-04-29 20:16:13 -0700

    Board renders!

--- src/Board.js
+++ src/Board.js
@@ -9,27 +9,36 @@ export default class Board extends Component {
     ).isRequired
   };
 
-  renderSquare(x , y) {
+  renderSquare(i) {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
     const black = ( x + y ) % 2 === 1;
     const [knightX, knightY] = this.props.knightPosition;
     const piece = ( x === knightX && y === knightY ) ? <Knight /> : null;
 
     return (
-      <Square black={black}>
-        {piece}
-      </Square>
+      <div key={i}
+           style={{ width: '12.5%', height: '12.5%' }}>
+        <Square black={black}>
+          {piece}
+        </Square>
+      </div>
     );
   }
 
   render() {
+    const squares = [];
+    for (let i = 0; i < 64; i++) {
+      squares.push(this.renderSquare(i));
+    }
     return (
       <div style={{
-        width: '300px',
-        height: '300px',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexWrap: 'wrap',
       }}>
-        {this.renderSquare(0,0)}
-        {this.renderSquare(1,0)}
-        {this.renderSquare(2,0)}
+        {squares}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import Board from './Board';
 
 ReactDOM.render(
-  <Board knightPosition={[0, 0]} />,
+  <Board knightPosition={[3, 7]} />,
   document.getElementById('root')
 );
