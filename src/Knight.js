823b7b9fcb6f5c1db46f166df1c45c9242a32474
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-04-29 23:29:15 -0700

    Drag and drop tutorial over! yay! That was fun :)

--- src/Knight.js
+++ src/Knight.js
@@ -1,7 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { ItemTypes } from './Constants';
+import { DragSource } from 'react-dnd';
 
-export default class Knight extends Component {
-  render() {
-    return <span>♘</span>;
+const knightSource = {
+  beginDrag(props) {
+    return {};
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging(),
   }
 }
+
+export default class Knight extends Component {
+  componentDidMount() {
+    const img = new Image();
+    img.src = 'blank';
+    img.onload = () => this.props.connectDragPreview(img);
+  }
+  render() {
+    const { connectDragSource, isDragging} = this.props;
+    return connectDragSource(
+      <div style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 35,
+        fontWeight: 'bold',
+        cursor: 'move',
+      }}>
+        ♘
+      </div>
+    );
+  }
+}
+
+Knight.propTypes = {
+  connectDragSource: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired,
+}
+
+export default DragSource(ItemTypes.KNIGHT, knightSource, collect)(Knight);
