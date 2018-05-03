db5868dc23f7a48782d368e1350b52d5516e4184
spacexengineer <hunter.casbeer@gmail.com>
2018-05-03 13:43:02 -0700

    tutorial finished and working

--- src/Board.js
+++ src/Board.js
@@ -1,98 +1,19 @@
-// import React, { Component, PropTypes } from 'react';
-// import { DragDropContext } from 'react-dnd';
-// import HTML5Backend from 'react-dnd-html5-backend';
-// import Square from './Square';
-// import BoardSquare from './BoardSquare';
-// import Knight from './Knight';
-// import { canMoveKnight, moveKnight } from './Game';
-
-// class Board extends Component {
-//   static propTypes = {
-//     knightPosition: PropTypes.arrayOf(
-//       PropTypes.number.isRequired
-//     ).isRequired
-//   };
-
-//   renderSquare(i) {
-//     const x = i % 8;
-//     const y = Math.floor(i / 8);
-
-//     return (
-//       <div
-//         key={i}
-//         style={{ width: '12.5%', height: '12.5%' }}
-//         onClick={() => this.handleSquareClick(x, y)}>
-//         <BoardSquare x={x} y={y}>
-//           {this.renderPiece(x, y)}
-//         </BoardSquare>
-//       </div>
-//     );
-//   }
-
-//   renderPiece(x, y) {
-//     const [knightX, knightY] = this.props.knightPosition;
-//     if (x === knightX && y == knightY) {
-//       return <Knight />;
-//     }
-//   }
-
-//   handleSquareClick(toX, toY) {
-//     if ( canMoveKnight(toX, toY)) {
-//       moveKnight(toX, toY);
-//     }
-//   }
-
-//   render() {
-//     const squares = [];
-//     for (let i = 0; i < 64; i++) {
-//       squares.push(this.renderSquare(i));
-//     }
-//     return (
-//       <div style={{
-//         width: '100%',
-//         height: '100%',
-//         display: 'flex',
-//         flexWrap: 'wrap',
-//       }}>
-//         {squares}
-//       </div>
-//     );
-//   }
-// }
-
-// // because of this http://babeljs.io/docs/plugins/transform-decorators/
-// export default DragDropContext(HTML5Backend)(Board);
-
-import React, { Component } from "react";
+import React, { Component, PropTypes } from "react";
 import { DragDropContext } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
-import PropTypes from "prop-types";
 import Square from "./Square";
+import BoardSquare from "./BoardSquare";
 import Knight from "./Knight";
-import { moveKnight, canMoveKnight } from "./Game";
+import { canMoveKnight, moveKnight } from "./Game";
 
 class Board extends Component {
-  // renderSquare(i) {
-  //   const x = i % 8;
-  //   const y = Math.floor(i / 8);
-  //   const black = (x + y) % 2 === 1;
+  static propTypes = {
+    knightPosition: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired
+  };
 
-  //   const [knightX, knightY] = this.props.knightPosition;
-  //   const piece = x === knightX && y === knightY ? <Knight /> : null;
-
-  //   return (
-  //     <div key={i} style={{ width: "12.5%", height: "12.5%" }}>
-  //       <Square black={black}>{piece}</Square>
-  //     </div>
-  //   );
-  // }
   renderSquare(i) {
     const x = i % 8;
     const y = Math.floor(i / 8);
-    const black = (x + y) % 2 === 1;
-
-    const [knightX, knightY] = this.props.knightPosition;
-    const piece = x === knightX && y === knightY ? <Knight /> : null;
 
     return (
       <div
@@ -100,13 +21,18 @@ class Board extends Component {
         style={{ width: "12.5%", height: "12.5%" }}
         onClick={() => this.handleSquareClick(x, y)}
       >
-        <Square black={black}>{piece}</Square>
+        <BoardSquare x={x} y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
       </div>
     );
   }
 
-  handleSquareClick(toX, toY) {
-    moveKnight(toX, toY);
+  renderPiece(x, y) {
+    const [knightX, knightY] = this.props.knightPosition;
+    if (x === knightX && y == knightY) {
+      return <Knight />;
+    }
   }
 
   handleSquareClick(toX, toY) {
@@ -114,12 +40,12 @@ class Board extends Component {
       moveKnight(toX, toY);
     }
   }
+
   render() {
     const squares = [];
     for (let i = 0; i < 64; i++) {
       squares.push(this.renderSquare(i));
     }
-
     return (
       <div
         style={{
@@ -135,8 +61,83 @@ class Board extends Component {
   }
 }
 
-Board.propTypes = {
-  knightPosition: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired
-};
-
+// because of this http://babeljs.io/docs/plugins/transform-decorators/
 export default DragDropContext(HTML5Backend)(Board);
+
+// import React, { Component } from "react";
+// import { DragDropContext } from "react-dnd";
+// import HTML5Backend from "react-dnd-html5-backend";
+// import PropTypes from "prop-types";
+// import Square from "./Square";
+// import Knight from "./Knight";
+// import { moveKnight, canMoveKnight } from "./Game";
+
+// class Board extends Component {
+//   // renderSquare(i) {
+//   //   const x = i % 8;
+//   //   const y = Math.floor(i / 8);
+//   //   const black = (x + y) % 2 === 1;
+
+//   //   const [knightX, knightY] = this.props.knightPosition;
+//   //   const piece = x === knightX && y === knightY ? <Knight /> : null;
+
+//   //   return (
+//   //     <div key={i} style={{ width: "12.5%", height: "12.5%" }}>
+//   //       <Square black={black}>{piece}</Square>
+//   //     </div>
+//   //   );
+//   // }
+//   renderSquare(i) {
+//     const x = i % 8;
+//     const y = Math.floor(i / 8);
+//     const black = (x + y) % 2 === 1;
+
+//     const [knightX, knightY] = this.props.knightPosition;
+//     const piece = x === knightX && y === knightY ? <Knight /> : null;
+
+//     return (
+//       <div
+//         key={i}
+//         style={{ width: "12.5%", height: "12.5%" }}
+//         onClick={() => this.handleSquareClick(x, y)}
+//       >
+//         <Square black={black}>{piece}</Square>
+//       </div>
+//     );
+//   }
+
+//   handleSquareClick(toX, toY) {
+//     moveKnight(toX, toY);
+//   }
+
+//   handleSquareClick(toX, toY) {
+//     if (canMoveKnight(toX, toY)) {
+//       moveKnight(toX, toY);
+//     }
+//   }
+//   render() {
+//     const squares = [];
+//     for (let i = 0; i < 64; i++) {
+//       squares.push(this.renderSquare(i));
+//     }
+
+//     return (
+//       <div
+//         style={{
+//           width: "100%",
+//           height: "100%",
+//           display: "flex",
+//           flexWrap: "wrap"
+//         }}
+//       >
+//         {squares}
+//       </div>
+//     );
+//   }
+// }
+
+// Board.propTypes = {
+//   knightPosition: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired
+// };
+
+// export default DragDropContext(HTML5Backend)(Board);
